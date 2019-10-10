d60ec191cc8c9957ea2e14d3331288a8ad579322
Muhammad Munir <MuhammadShaMunir@gmail.com>
2019-10-10 11:10:04 -0500

    first commit for the project

---
+++ src/main/java/com/muhammad/events/MuhammadMunirEventsBeltReviewerApplication.java
@@ -0,0 +1,13 @@
+package com.muhammad.events;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuhammadMunirEventsBeltReviewerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuhammadMunirEventsBeltReviewerApplication.class, args);
+	}
+
+}
